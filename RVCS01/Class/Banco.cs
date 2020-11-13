b4a068b04609de97f337413cfdbbf5b9df2f590f
isabella.scosta2 <isabella.scosta2@ITQ0586766W10-1>
2020-11-13 09:45:28 -0300

    Preparação de ambiente,criação e implementação de classe Banco

---
+++ RVCS01/Class/Banco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace RVCS01.Class
+{
+    public static class Banco
+    {//atributo
+        //metodo
+        public static MySqlCommand Abrir() 
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            string strcon = @"Server = 10.91.43.20;user id=root; Database=myescolarapp; Pwd=@Senac;";
+            MySqlConnection cn = new MySqlConnection(strcon);
+            cmd.Connection = cn;
+            return cmd;
+        }
+
+    }
+}
--- RVCS01/Views/FrmCurso.cs
+++ RVCS01/Views/FrmCurso.cs
@@ -16,5 +16,10 @@ namespace RVCS01
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
